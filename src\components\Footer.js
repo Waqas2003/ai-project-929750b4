a9f737210e80379623ca9c12d2dfaad5050eaefa
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 00:39:25 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-gray-200 h-screen py-12">
+      <p className="text-lg text-gray-600">
+        &copy; 2023 Pakistan Atomic Power. All rights reserved.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
